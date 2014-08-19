2b4f5381abfee66e91a3ae05f25167847593bbbc
Heath Dutton <heathdutton@gmail.com>
2014-08-19 15:39:43 -0400

    .

--- bake.sh
+++ bake.sh
@@ -31,8 +31,13 @@ then
 fi
 composer install --no-interaction --prefer-dist --no-dev
 
+cd vendor
+cd bin
+ls
+
+
 echo "Building the stub make file into a new drupal folder."
-php ./vendor/bin/drush.php make bakery-stub.make drupal_new --force-complete --md5 --working-copy --prepare-install
+php vendor/bin/drush.php make bakery-stub.make drupal_new --force-complete --md5 --working-copy --prepare-install
 
 echo "Dropping the auto-generated sites/default folder"
 rm -rf drupal_new/sites/default
