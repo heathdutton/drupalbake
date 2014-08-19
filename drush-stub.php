31f15a1197bbc27fd7a5943eb377ae86c58ca09f
Heath Dutton <heathdutton@gmail.com>
2014-08-19 16:44:40 -0400

    .

--- drush-stub.php
+++
@@ -1,2 +0,0 @@
-<?php
-exec('php -f vendor/bin/drush.php make bakery-stub.make drupal_new --force-complete --md5 --working-copy --prepare-install');
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/php
-
-echo ":::::::::::::::::::::::: DRUPAL BAKING START :::::::::::::::::::::::::"
-
-php -f drush-stub.php
-
-echo "::::::::::::::::::::::::: DRUPAL BAKING END ::::::::::::::::::::::::::"
-
