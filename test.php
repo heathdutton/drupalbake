3efdd4ec9c1f4109d97db532749947faba2ccdf8
Heath Dutton <heathdutton@gmail.com>
2014-08-20 00:29:27 -0400

    .

--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor' . '/autoload.php';
-require_once __DIR__ . '/vendor/composer' . '/autoload_real.php';
-require_once __DIR__ . '/vendor/drush/drush/drush.php';
